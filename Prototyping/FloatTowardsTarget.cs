413ec90df7c024bd7149c449d70b614f2a1121bb
Dragonfoxing <mythsotech@gmail.com>
2020-02-19 15:07:20 -0600

    Added more prototype tools

---
+++ Prototyping/FloatTowardsTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dragonfoxing.UnityCSharp
+{
+    public class FloatTowardsTarget : GameEntity
+    {
+        public Transform target;
+        public float gravitySpeed = 1f;
+
+        public void Update()
+        {
+            if (!Paused)
+            {
+                Vector3 dir = target.position - transform.position;
+                
+                transform.position += (gravitySpeed * Time.deltaTime * dir.normalized);
+            }
+        }
+
+        
+    }
+
+}
