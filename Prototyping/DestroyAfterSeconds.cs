b1471ab29a6995da48007da8481bb3b6b421d8d8
Dragonfoxing <mythsotech@gmail.com>
2020-03-08 17:06:00 -0500

    Added DestroyAfterSeconds
    Added another prototyping tool.

---
+++ Prototyping/DestroyAfterSeconds.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyAfterSeconds : MonoBehaviour
+{
+    public float delayTimer = 3f;
+
+    private void OnEnable()
+    {
+        StartCoroutine(ExecuteAfterTime(delayTimer));
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(ExecuteAfterTime(delayTimer));
+    }
+
+    private void OnApplicationQuit()
+    {
+        StopCoroutine(ExecuteAfterTime(delayTimer));
+    }
+
+    private void OnDestroy()
+    {
+        StopCoroutine(ExecuteAfterTime(delayTimer));
+    }
+    
+    IEnumerator ExecuteAfterTime(float n)
+    {
+        yield return new WaitForSeconds(n);
+        Destroy(gameObject);
+    }
+}
